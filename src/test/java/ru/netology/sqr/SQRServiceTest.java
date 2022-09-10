a7a912b6e22f431107ee87c89771e417ac7b04e1
hellen0874 <en.musinova@gmail.com>
2022-09-10 22:13:29 +0300

    import for ParameterizedTest added

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,6 +2,8 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
 // import org.junit.jupiter.params.provider.CsvSource;
 
 
